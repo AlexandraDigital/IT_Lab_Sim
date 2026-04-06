1dfa4f2a0a5de4c41efb1718f0078edfbbb566d9
AlexandraDigital <futuresuccess105@gmail.com>
2026-04-06 16:42:18 -0400

    Update Inventory.jsx

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,4 +1,6 @@
+// src/components/Inventory.jsx
 import React from 'react';
+import { images } from '../assets/images';
 
 export default function Inventory({ inventory, addToBuild }) {
   return (
@@ -7,9 +9,18 @@ export default function Inventory({ inventory, addToBuild }) {
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {inventory.map((comp, i) => (
           <div key={i} className="card">
+            <img
+              src={images[comp.type]}
+              alt={comp.type}
+              width={64}
+              height={64}
+              style={{ marginBottom: '8px' }}
+            />
             <strong>{comp.type}</strong>: {comp.name}
             <br />
-            <button onClick={() => addToBuild(comp)}>Add to Build</button>
+            <button onClick={() => addToBuild(comp)} style={{ marginTop: '8px' }}>
+              Add to Build
+            </button>
           </div>
         ))}
       </div>
