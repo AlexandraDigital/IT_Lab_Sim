cd06988ada617403483a9e76c2884acab131a385
AlexandraDigital <futuresuccess105@gmail.com>
2026-04-06 16:42:42 -0400

    Update Build.jsx

--- src/components/Build.jsx
+++ src/components/Build.jsx
@@ -1,4 +1,6 @@
+// src/components/Build.jsx
 import React from 'react';
+import { images } from '../assets/images';
 
 export default function Build({ build, removeFromBuild }) {
   return (
@@ -7,9 +9,18 @@ export default function Build({ build, removeFromBuild }) {
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {build.map(comp => (
           <div key={comp.id} className="card">
+            <img
+              src={images[comp.type]}
+              alt={comp.type}
+              width={64}
+              height={64}
+              style={{ marginBottom: '8px' }}
+            />
             <strong>{comp.type}</strong>: {comp.name}
             <br />
-            <button onClick={() => removeFromBuild(comp.id)}>Remove</button>
+            <button onClick={() => removeFromBuild(comp.id)} style={{ marginTop: '8px' }}>
+              Remove
+            </button>
           </div>
         ))}
       </div>
