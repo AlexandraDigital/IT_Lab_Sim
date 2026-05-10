e2b2bff4690e447fc4be33df050d85d4aa54153b
AlexandraDigital <futuresuccess105@gmail.com>
2026-05-09 21:03:46 -0400

    Delete src directory

--- src/assets/images.js
+++
@@ -1,7 +0,0 @@
-export const images = {
-  CPU: "https://cdn-icons-png.flaticon.com/512/2721/2721264.png",
-  Motherboard: "https://cdn-icons-png.flaticon.com/512/3659/3659898.png",
-  RAM: "https://cdn-icons-png.flaticon.com/512/633/633611.png",
-  GPU: "https://cdn-icons-png.flaticon.com/512/3659/3659899.png",
-  Storage: "https://cdn-icons-png.flaticon.com/512/1042/1042390.png"
-};
